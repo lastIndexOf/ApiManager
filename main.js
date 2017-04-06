a37cedbd12508a07ad2f392a028d47e87973eda0
lastIndexOf <kgaikj2cu@icloud.com>
2017-04-06 20:01:19 +0800

    git status

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { 
   app, 
   BrowserWindow,
+  globalShortcut,
   ipcMain: ipc
 } = require('electron')
 
@@ -8,15 +9,20 @@ let win = null
 
 app.on('ready', createWindow)
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
-
-ipc.on('online-status-changed', (e, status) => {
-  console.log(status)
+app.on('before-quit', e => {
+  console.log(e)
 })
-
+app.on('browser-window-blur', (e, window) => {
+  console.log(`blur: ${ window }`)
+})
+app.on('browser-window-focus', (e, window) => {
+  console.log(`focus: ${ window }`)
+})
+app.on('browser-window-created', (e, window) => {
+  console.log(3)
+})
 /*
   操作函数
 */
@@ -24,9 +30,17 @@ function createWindow() {
   win = new BrowserWindow({ width: 800, hgith: 600 })
 
   win.loadURL(`file://${ __dirname }/index.html`)
-  
+
   win.openDevTools()
+  
+  registerKeys()
   win.on('closed', () => {
     win = null
+  })
+}
+
+function registerKeys() {
+  globalShortcut.register('Ctrl+3', () => {
+    console.log(1)
   })
 }
